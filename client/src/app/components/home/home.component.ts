a573426ea57e76c018dc0fdf39a675a24ed60b9d
Juan Miguel Rojas <juan.rojas@pragma.com.co>
2024-06-11 16:47:56 -0500

    Se agrega la base de los Graficos con Apex Chart

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Cantidad",
+          data: [3, 5, 0, 2]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "Estados de las Obras"
+      },
+      xaxis: {
+        categories: ["Asignada", "En Desarrollo", "En Revisión", "Aceptada"]
+      }
+    };
+  }
 }
